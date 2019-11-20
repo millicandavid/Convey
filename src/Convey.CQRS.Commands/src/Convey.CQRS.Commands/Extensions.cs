658de56e576a7063a2680ee5428b40eff521463e
millicandavid <david.millican@live.com>
2019-11-20 09:48:10 -0600

    allowing a filter on Assembly by name to avoid issues found in unit testing

--- src/Convey.CQRS.Commands/src/Convey.CQRS.Commands/Extensions.cs
+++ src/Convey.CQRS.Commands/src/Convey.CQRS.Commands/Extensions.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using Convey.CQRS.Commands.Dispatchers;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -6,16 +7,17 @@ namespace Convey.CQRS.Commands
 {
     public static class Extensions
     {
-        public static IConveyBuilder AddCommandHandlers(this IConveyBuilder builder)
+        public static IConveyBuilder AddCommandHandlers(this IConveyBuilder builder, string rootNamespace = null)
         {
             builder.Services.Scan(s =>
-                s.FromAssemblies(AppDomain.CurrentDomain.GetAssemblies())
+                s.FromAssemblies(AppDomain.CurrentDomain.GetAssemblies()
+                        .Where(a => (rootNamespace == null) ? true : a.FullName.StartsWith(rootNamespace)))
                     .AddClasses(c => c.AssignableTo(typeof(ICommandHandler<>)))
                     .AsImplementedInterfaces()
                     .WithTransientLifetime());
 
             return builder;
-        }
+        }        
 
         public static IConveyBuilder AddInMemoryCommandDispatcher(this IConveyBuilder builder)
         {
